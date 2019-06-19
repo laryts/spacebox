b6fa37647b31b95f9177b5d17f2de80896a6a610
Larissa Soares <lari_soares9@hotmail.com>
2019-06-19 00:33:18 -0300

    Add comments

--- js/index.js
+++ js/index.js
@@ -6,19 +6,25 @@ const containerSelected = $(".container-selected");
 const contentstaffSelected = $(".staff-selected");
 let data;
 
+// Listing event click
 document.addEventListener(
   "click",
   function(event) {
+    event.preventDefault();
+
+    // Looking for all selects to disable active class
     document.querySelectorAll(".card").forEach(item => {
       if (item.classList.contains("active")) {
         item.classList.remove("active");
       }
     });
+
     if (!event.target.matches(".card")) return;
-    event.preventDefault();
+
+    // Toggle class active
     event.target.classList.toggle("active");
 
-    //
+    // Get id of staff selected and find in data to render template selected
     const key = event.target.getAttribute("key");
     data.find(function(staff, index) {
       if (staff.id == key) {
@@ -29,6 +35,7 @@ document.addEventListener(
   false
 );
 
+// Build template staff selected
 const templateSeleted = staff => {
   return `
     <div class="card" id="staff-${staff.id}" key="${staff.id}">
@@ -51,6 +58,7 @@ const templateSeleted = staff => {
     `;
 };
 
+// Build template staff's list
 const template = staffs => {
   return `
   ${staffs
@@ -71,12 +79,16 @@ const template = staffs => {
     .join(" ")} `;
 };
 
+// Get data of json file
 fetch("dados.json")
   .then(response => {
     return response.json();
   })
   .then(staffs => {
+    // global variable
     data = staffs;
+
+    // add staffs list in HTML > container-staffs
     contentstaff.innerHTML = template(staffs);
   })
   .catch(err => console.error(err));
